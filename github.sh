e7045012eda57146f98cbebfb838a9572a9359cb
JeyF123 <jeyf123@gmail.com>
2021-01-30 04:11:45 +0100

    Trigger rebuild

---
+++ github.sh
@@ -0,0 +1,17 @@
+ #!/bin/bash
+git init
+sleep 2
+git add .
+sleep 2
+git branch -M main
+sleep 2
+git remote add origin https://github.com/JeyF123/lmac1.git
+sleep 2
+git push -u origin main
+sleep 2
+git commit --allow-empty -m "Trigger rebuild"
+sleep 10
+git push
+sleep 20
+chromium https://jeyf123.github.io/lmac1/
+
